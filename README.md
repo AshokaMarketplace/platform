5a9bcd3bd838098aad8217761dae3b81e3378702
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2025-03-01 19:21:42 +0530

    INITIAL COMMIT

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ashoka Marketplace
+
+Source code for the Ashoka Marketplace web platform.
---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+exports.landing = (req, res, next) => {
+	res.render("index", { title: "Ashoka Marketplace" });
+}
